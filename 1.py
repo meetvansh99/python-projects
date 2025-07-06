cfcc6685116c029597dad19fdec2c3e07fed8bc9
meetvansh99 <meetvansh27@gmail.com>
2025-07-06 23:32:04 +0530

    quize

---
+++ 1.py
@@ -0,0 +1,20 @@
+print("Wel come to our gf quiz game😊")
+
+answer=input("Do you want to play? ")
+
+if answer!="yes":
+    quit()
+
+print("Okay let's play😘")    
+
+q_1=input("what is your gf fav color:")
+if q_1=="pink":
+    print("Correct🤗!")
+else:
+    print("You wrong mf🤬")
+
+q_2=input("What is your birth date of your gf😁:")
+if q_2=="7november":
+     print("Correct🤗!")
+else:
+    print("You wrong mf🤬")
